5e894733182e4c8280b77fa7fc98dae5cc6802c3
Murad Alam <68848774+Kasturi786@users.noreply.github.com>
2025-06-06 21:26:42 +0200

    slightly change in index file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello Watchtower, this time download recent build image and update the running container!</h1>')
 })
 
 const PORT = 8080
